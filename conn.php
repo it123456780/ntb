a5c247a896e08b71c79ba30bd3a205f757adbf38
it123456780 <0963478360t>
2023-01-12 11:48:29 +0700

    new file

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
     // Create connection
-    $conn = mysqli_connect("127.0.0.1","it65309010015","it12345","province");
+    $conn = mysqli_connect("202.29.229.253","it65309010015","it12345","province");
     mysqli_set_charset($conn,"utf8");
     // Check connection
     if (!$conn) {
